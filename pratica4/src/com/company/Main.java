2b8e19409c3700b96f293d86f3df912367e28aae
abeja21 <74176323+abeja21@users.noreply.github.com>
2021-01-24 19:12:53 +0100

    Entrar a Amazon y buscar el modelo

--- pratica4/src/com/company/Main.java
+++ pratica4/src/com/company/Main.java
@@ -6,17 +6,22 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Scanner;
+
 public class Main {
 private static WebDriver driver;
     public static void main(String[] args) {
-        String exePath = "C:\\Users\\angel\\IdeaProjects\\pratica4\\chromedriver.exe";
+        Scanner sc= new Scanner(System.in);
+        String exePath = "C:\\Users\\Mario Beltran Garcia\\IdeaProjects\\pratica4\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",exePath);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
-        driver.get("http://www.google.es");
-     WebElement element= driver.findElement(By.name("q"));
-     element.sendKeys("amazon");
+        driver.get("http://www.amazon.es");
+     WebElement element= driver.findElement(By.id("twotabsearchtextbox"));
+     System.out.println("Dime el modelo que deseas buscar");
+     String modelo=sc.nextLine();
+     element.sendKeys("telefono"+" "+modelo);
     element.submit();
        WebDriverWait waiting= new WebDriverWait(driver,10);
 
