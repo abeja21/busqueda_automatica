55d64911a1b91df21b8e8c29a942a792947af03b
abeja21 <74176323+abeja21@users.noreply.github.com>
2021-01-25 11:30:36 +0100

    primer intento de lectura de datods

--- pratica4/src/com/company/Main.java
+++ pratica4/src/com/company/Main.java
@@ -12,18 +12,26 @@ public class Main {
 private static WebDriver driver;
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        String exePath = "C:\\Users\\Mario Beltran Garcia\\IdeaProjects\\pratica4\\chromedriver.exe";
+        String exePath = "chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",exePath);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
-        driver.get("http://www.amazon.es");
+        driver.get("https://www.amazon.es/gp/browse.html?node=931491031&ref_=nav_em__tele_0_2_13_3");
      WebElement element= driver.findElement(By.id("twotabsearchtextbox"));
      System.out.println("Dime el modelo que deseas buscar");
      String modelo=sc.nextLine();
-     element.sendKeys("telefono"+" "+modelo);
+     element.sendKeys(modelo);
     element.submit();
-       WebDriverWait waiting= new WebDriverWait(driver,10);
+        System.out.println("dime la cantidad de modelos quiere que imprima");
+        int cantidad=2;
+
+  driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[2]/div["+cantidad+"]"+"/div/span/div/div/div[4]/div/div/div/a/span/span[2]/span[1]"));
+            System.out.println(1+"--"+driver.getTitle());
+driver.quit();
+        }
+
+
 
     }
-}
+
